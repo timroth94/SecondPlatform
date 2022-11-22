50812c4a2338223486559e923cf9df6a9aa2b09a
timroth94 <jiaweizhou865@gmial.com>
2022-11-22 23:25:25 +0800

    update

--- pages/Selfchoice/Ksetting.vue
+++ pages/Selfchoice/Ksetting.vue
@@ -23,7 +23,20 @@
 			<text>操作</text>
 		</view>
 	</view>
-
+	<view class="setting-outter">
+		<template v-for="(item,index) in K_Setting" :key="index">
+			<view class="setting-inner">
+			<view class="setting-name">
+				<text>{{item.name_en}}</text>
+				<text>{{item.name_cn}}</text>
+			</view>
+			<view class="">
+				<switch :checked="item.isshow" @change="" style="transform:scale(0.8)" color="rgba(255, 94, 59, 1)"/>
+				<text>{{item.action}}</text>
+			</view>
+			</view>
+		</template>
+	</view>
 
 
 
@@ -34,7 +47,39 @@
 	import {
 		ref
 	} from "vue";
-
+	
+	const K_Setting = ref([
+		{
+			name_en: 'MACD',
+			name_cn: '（平滑异同曲线）',
+			isshow: true,
+			action: '设参数'
+		},
+		{
+			name_en: 'VOL',
+			name_cn: '（成交量）',
+			isshow: true,
+			action: '设参数'
+		},
+		{
+			name_en: 'KDJ',
+			name_cn: '（随机指标）',			
+			isshow: false,
+			action: '设参数'
+		},
+		{
+			name_en: 'RSI',
+			name_cn: '（相对强弱指标）',			
+			isshow: true,
+			action: '？'
+		},
+		{
+			name_en: 'BOLL',
+			name_cn: '（布林线）',			
+			isshow: true,
+			action: '？'
+		}
+	])
 	const title = ['分时', 'K线']
 	const topbar = [1, 2, 3]
 	const activevalue = ref(0)
@@ -114,5 +159,44 @@
 	}
 	.setting-bottom> :nth-child(2) > :nth-child(2){
 		margin-right: 180rpx;
+	}
+	
+	.setting-outter{
+		display: flex;
+		flex-direction: column;
+		margin: 26rpx 32rpx 0;
+	}
+
+	.setting-inner{
+		display: flex;
+		align-items: center;
+		padding: 26rpx 0 24rpx;
+		border-bottom: 1rpx rgb(242, 242, 242) solid;
+	}
+	
+	.setting-name > :nth-child(1){
+		font-size: 30rpx;
+	}
+	.setting-name > :nth-child(2){
+		font-size: 28rpx;
+		color: rgba(153, 153, 153, 1);
+	}
+	
+	.setting-inner > :nth-child(2){
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		flex: 6;
+		font-size: 28rpx;
+		color: rgba(153, 87, 87, 1);
+	}
+	.setting-inner > :nth-child(1){
+		flex: 7;
+	}
+	
+	.setting-outter > :nth-child(4) > :nth-child(2) text,
+	.setting-outter > :nth-child(5) > :nth-child(2) text
+	{
+		margin-right: 26rpx;
 	}
 </style>
